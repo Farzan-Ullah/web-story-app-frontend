1a2b68ead288e3e6fd2594a470de331c7c1c3eb6
farzan <farzanullah07@gmail.com>
2024-04-22 03:45:08 +0530

    Buttons Modal Added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Navbar.module.css";
+import Modal from "../Modal/Modal";
+
 export default function Navbar() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div>
       <nav className={styles.navbar}>
         <div className={styles.logo}>SwipTory</div>
         <div className={styles.navBtns}>
-          <button className={styles.registerBtn}>Register Now</button>
+          <button className={styles.registerBtn} onClick={openModal}>
+            Register Now
+          </button>
           <button className={styles.loginBtn}>Sign In</button>
         </div>
       </nav>
+      <Modal isOpen={isModalOpen} onClose={closeModal} />
       <br />
     </div>
   );
