e0ed6d971f39f6fce0d4c3669f3c9ec83996c11f
farzan <farzanullah07@gmail.com>
2024-05-02 00:09:31 +0530

    updated

--- src/apis/userAuth.js
+++ src/apis/userAuth.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const backendUrl = `http://localhost:3001/api`;
+const backendUrl = `https://swiptory-web-story-app-backend.onrender.com`;
 
 export const registerUser = async ({ username, password }) => {
   try {
